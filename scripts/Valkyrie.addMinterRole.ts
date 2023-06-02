6904a91e0dfb87a36cb4824bda9b7096fe2170b5
Sim Young Jae <hanul@hanul.me>
2023-06-02 22:56:23 +0900

    Update Valkyrie.addMinterRole.ts

--- scripts/Valkyrie.addMinterRole.ts
+++ scripts/Valkyrie.addMinterRole.ts
@@ -6,7 +6,7 @@ async function main() {
     const valkyrie = Valkyrie.attach("0x480CEffe657cf6BD19d9C0171D42B73B6fFc2284");
 
     // minter
-    await valkyrie.grantRole("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", "0xd28e5B1bbFAF027cdda697a682e845376120FE56");
+    await valkyrie.grantRole("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", "0xBdb162C3B746D9737Bd31281Cd43090aE1986812");
 
     console.log(`Valkyrie address: ${valkyrie.address}`);
 }
